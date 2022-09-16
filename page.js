c6bb7ffe9c285c1c5dbf18870a4dc44e106c3a51
Kadice <kadice@ys-infoware.com>
2022-09-17 00:10:30 +0800

    fix bug

--- page.js
+++ page.js
@@ -3,9 +3,8 @@
     let sep = window.location.href.lastIndexOf('/');
     let base = location.href.substring(0, sep + 1);
     let page = location.href.substring(sep + 1);
-    if (page.indexOf('.html' > 0) && page.indexOf('index.html') < 0) {
+    if ((page.indexOf('.html') > 0) && (page.indexOf('index.html') < 0)) {
         let url = base + ((window.origin == 'file://') ? "index.html" : "") + "?to=" + page;
-        console.log(url);
-        //window.location.href = url;
+        window.location.href = url;
     }
 }
