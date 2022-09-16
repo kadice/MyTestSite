daab2208c690537cd10de0dbc0f0ed116c1e4ffb
Kadice <kadice@ys-infoware.com>
2022-09-16 23:48:57 +0800

    v0.2

---
+++ page.js
@@ -0,0 +1,11 @@
+{
+    //子頁面網址重新導向回母頁面
+    let sep = window.location.href.lastIndexOf('/');
+    let base = location.href.substring(0, sep + 1);
+    let page = location.href.substring(sep + 1);
+    if (page.indexOf('.html' > 0) && page.indexOf('index.html') < 0) {
+        let url = base + ((window.origin == 'file://') ? "index.html" : "") + "?to=" + page;
+        console.log(url);
+        window.location.href = url;
+    }
+}
